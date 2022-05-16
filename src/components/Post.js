be3663904c7bf85c888f39473d2f12d3518b21a0
CodeDragi <codedragi@gmail.com>
2022-05-16 15:01:21 +0200

    edit message ok

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,38 +1,64 @@
-import React from 'react';
+import { doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../utils/firebase.config";
 
 const Post = ({ post, user }) => {
-    const dateFormater = (date) => {
-        let days = Math.floor((new Date() - new Date(date)) / (1000 * 3600 * 24));
+  const [edit, setEdit] = useState(false);
+  const [editMess, setEditMess] = useState(null);
 
-        if (days === 0) {
-            return "aujourd'hui";
-        } else if (days === 1) {
-            return "il y a 1 jour";
-        } else {
-            return "il y a " + days + " jours";
-        }
+  const dateFormater = (date) => {
+    let days = Math.floor((new Date() - new Date(date)) / (1000 * 3600 * 24));
+
+    if (days === 0) {
+      return "aujourd'hui";
+    } else if (days === 1) {
+      return "il y a 1 jour";
+    } else {
+      return "il y a " + days + " jours";
     }
-    return (
-        <div className='post'>
-            <div className='post-header'>
-                <div className='left-part'>
-                    <div className='title'>
-                        <span>{post.author[0]}</span>
-                        <h2>{post.author}</h2>
-                    </div>
-                    <h5>Posté {dateFormater(post.date)}</h5>
-                </div>
-                {post.authorId === user?.uid && (
-                    <div className='right-part'>
-                    <span><i className='fa-solid fa-pen-to-square'></i></span>
-                    <span>DELETE</span>
-                </div>
-                )}
-                
-            </div>
-            <p>{post.message}</p>
+  };
+
+  const handleEdit = () => {
+      setEdit(false);
+
+      if (editMess) {
+        updateDoc(doc(db, "posts", post.id), { message: editMess })
+      }
+  }
+
+  return (
+    <div className="post">
+      <div className="post-header">
+        <div className="left-part">
+          <div className="title">
+            <span>{post.author[0]}</span>
+            <h2>{post.author}</h2>
+          </div>
+          <h5>Posté {dateFormater(post.date)}</h5>
         </div>
-    );
+        {post.authorId === user?.uid && (
+          <div className="right-part">
+            <span onClick={() => setEdit(!edit)}>
+              <i className="fa-solid fa-pen-to-square"></i>
+            </span>
+            <span>DELETE</span>
+          </div>
+        )}
+      </div>
+      {edit ? (
+        <React.Fragment>
+          <textarea
+            autoFocus
+            value={editMess ? editMess : post.message}
+            onChange={(e) => setEditMess(e.target.value)}
+          ></textarea>
+          <button className="edit-btn" onClick={() => handleEdit()}>Valider</button>
+        </React.Fragment>
+      ) : (
+        <p>{editMess ? editMess : post.message}</p>
+      )}
+    </div>
+  );
 };
 
 export default Post;
